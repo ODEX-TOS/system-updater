be101e87bfdc9b41580493804dfdd016ef37affc
F0xedb <tom@odex.be>
2019-11-14 22:36:20 +0100

    Added base for the tool

--- system-updater.sh
+++ system-updater.sh
@@ -1,24 +1,42 @@
 #!/bin/bash
 
+# color codes
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BLUE='\033[0;35m'
+ORANGE='\033[1;33m'
+NC='\033[0m' # No Color
+
+name="system-updater"
+version="v0.1"
+
 
 function help {
-
+        printf "${ORANGE} $name ${NC}OPTIONS: difference | help | info | version\n\n" 
+        printf "${ORANGE}USAGE:${NC}\n"
+        printf "$name  \t\t\t Update your current system\n"
+        printf "$name  --difference \t\t Show what is new in the latest tos version\n"
+        printf "$name  --help \t\t\t Show this help message\n"
+        printf "$name  --info \t\t\t Show your current tos version\n"
+        printf "$name  --version \t\t Show information about this tool\n"
 }
 
-function version {
 
+
+function version {
+    printf "${ORANGE}TOS $name${NC} - Updating software\n${ORANGE}Version: ${NC}${version}\n"
 }
 
 function difference {
-
+    printf "Difference\n"
 }
 
 function update {
-
+    printf "updating\n"
 }
 
 function info {
-
+    printf "Current tos version %s\n" "$(cat /etc/version)"
 }
 
 case "$1" in 
